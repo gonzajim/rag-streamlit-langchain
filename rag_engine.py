128fd47c9751625af316205932597fe0fa2744a7
gonzalojm <gjimenez@jimarsoft.com>
2024-03-23 09:21:10 +0100

    gfgdf

--- rag_engine.py
+++ rag_engine.py
@@ -67,8 +67,8 @@ def process_documents():
             embeddings = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=os.environ['OPENAI_API_KEY'])
             db = FAISS.from_documents(all_chunks, embeddings)
             st.write(f"Indice de FAISS: {db.index.ntotal}")
-            docsearch = save_embeddings_to_mongo(all_chunks, embeddings, index_name="uclm_corpus")
-            st.write(f"Indice de FAISS: {docsearch.search_by_vector(embeddings[1], top_k=10)}")
+            #docsearch = save_embeddings_to_mongo(all_chunks, embeddings, index_name="uclm_corpus")
+            #st.write(f"Indice de FAISS: {docsearch.search_by_vector(embeddings[1], top_k=10)}")
         except Exception as e:
             st.error(f"An error occurred while retrieving embeddings: {e}")
 
